32f25fa2ea7c82d236f1a54775a40e4f2c73876a
Jdawson123 <jadawson@utc4me.org>
2015-09-16 10:03:57 -0400

    First Commit
    aasdf

---
+++ Assets/Scripts/BulletFireScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BulletFireScript : MonoBehaviour 
+{
+	public float fireTime = 1.0f;
+	public Transform Spawn;
+	
+	void Update () 
+	{
+		if (Input.GetButtonDown ("Fire1")) 
+		{
+			Fire();
+		}
+
+	}
+	
+
+	void Fire () 
+	{
+		GameObject obj = NewObjectpoolerScript.current.GetPooledObject ();
+		if (obj == null)
+			return;
+
+		obj.transform.position = Spawn.transform.position;
+		obj.transform.eulerAngles = new Vector3 (transform.rotation.x, 0, 0);
+		obj.SetActive(true);
+
+		obj.GetComponent<Rigidbody> ().AddForce (transform.localPosition * 500);
+
+
+
+	}
+}
---
+++ Assets/Scripts/BulletScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletScript : MonoBehaviour 
+{
+	public float speed = 5;
+	public Rigidbody Bullet;
+
+	void Start ()
+	{
+
+	}
+
+}
